e63732422c448425ee4a5be6a8656ba3b3b8b7d4
Nikhil Bhardwaj <127859341+Bharnikhil@users.noreply.github.com>
2024-03-13 16:23:06 +0530

    Add files via upload

---
+++ Comparing/MainStudent.java
@@ -0,0 +1,8 @@
+package Comparing;
+
+public class MainStudent {
+    public static void main(String[] args) {
+        Student Nikhil = new Student(77,90);
+        Student Rishabh = new Student(77,90);
+    }
+}
